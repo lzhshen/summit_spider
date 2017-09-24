d05b9772390c4aad6abb00928f091692898621b6
lzhshen <shen@localhost.localdomain>
2017-09-23 20:41:34 -0400

    be able to crawl fields

--- summit_spider/items.py
+++ summit_spider/items.py
@@ -11,12 +11,24 @@ import scrapy
 class SummitItem(scrapy.Item):
   # define the fields for your item here like:
   # name = scrapy.Field()
+  base_fname = scrapy.Field()
   title = scrapy.Field()
   speakers = scrapy.Field()
-  category = scrapy.Field()
+  tag = scrapy.Field()
   desc = scrapy.Field()
-  video_link = scrapy.Field()
-  video_dl_link = scrapy.Field()
-  slide_link = scrapy.Field()
+  video = scrapy.Field()
+  slide = scrapy.Field()
 
-  pass
+class SpeakerItem(scrapy.Item):
+  name = scrapy.Field()
+  corp = scrapy.Field()
+  bio = scrapy.Field()
+
+class VideoItem(scrapy.Item):
+  src_link = scrapy.Field()
+  dl_link = scrapy.Field()
+ 
+class SlideItem(scrapy.Item):
+  src_link = scrapy.Field()
+  dl_link = scrapy.Field()
+
--- summit_spider/spiders/kafka-summit-sf17.py
+++ summit_spider/spiders/kafka-summit-sf17.py
@@ -1,7 +1,8 @@
 import scrapy
 from scrapy.http import Request
 from scrapy.loader import ItemLoader
-from summit_spider.items import SummitItem
+from summit_spider.items import *
+import re
 
 def gen_cookie_map(cstr):
   cmap = {}
@@ -10,16 +11,29 @@ def gen_cookie_map(cstr):
     cmap[key.strip()] = value.strip()
   return cmap
 
+def extract_speaker_info(speaker_str):
+  (name, corp) = speaker_str.rsplit(',', 1)
+  return (name.strip(), corp.strip())
+
+def extract_basename(video_link):
+  basename = ''
+  l = re.findall('.*/video/(\d*)?.*', video_link)
+  if len(l) > 0: 
+    basename = l[0]
+  return basename
+     
+
 class KafkaSpider(scrapy.Spider):
   name = "kafka-summit-sf17"
   start_urls = [
     'file:///home/shen/mydev/summit_spider/summit_spider/html/kafka-summit-sf17.html',
   ]
   confluent_cookie_str = "__atuvc=2%7C47; __lc.visitor_id.7950501=S1487168434.fe4d62bb33; __hstc=165858785.bed41108038406d514d1be21f9a56214.1480056839886.1493088572462.1493093705403.6; hubspotutk=bed41108038406d514d1be21f9a56214; printfriendly-font-class=pf-15; kafka_summit_sf=yes; _mkto_trk=id:582-QHX-262&token:_mch-www.confluent.io-1505978200408-56477; popup_form_thank_you=null; _vwo_uuid_v2=CC4A3BA15954A517E3FDEA59007E4268|92891eab52a41c14bf1796087f69bbb7; _ga=GA1.2.1019333526.1505978203; _gid=GA1.2.599540853.1505978203; _hjIncludedInSample=1; lc_window_state=minimized; autoinvite_callback=true; autoinvite_callback=true"
-  sections = ["systems-track",
-#              "streams-track",
-#              "pipeline-track",
-#              "use-case-track",
+  sections = [
+              "systems-track",
+              "streams-track",
+              "pipeline-track",
+              "use-case-track",
              ]
 
   def parse(self, response):
@@ -36,30 +50,52 @@ class KafkaSpider(scrapy.Spider):
       
 
     # extract other sections
-
     for s in self.sections:
       for topic in response.xpath("//section[@id=$sectionId]/div/ul/li", sectionId=s):
-        item = SummitItem()
+        summit = SummitItem()
         li = topic.xpath("div[@class='resource_description']/div//text() | div[@class='resource_description']//text()").extract()
         desc = ''.join(li).strip()
         video_link = topic.xpath("div[@class='right']/a/@href").extract_first().strip()
-        item['title'] = topic.xpath(".//div/a/p/text()").extract_first().strip()
-        item['speakers'] = topic.xpath("div/p/text()").extract_first().strip()
-        item['desc'] = desc
-        item['category'] = s
+        summit['title'] = topic.xpath(".//div/a/p/text()").extract_first().strip()
+        summit['desc'] = desc
+        summit['tag'] = s
+        summit['speakers'] = []
+        speakers_str = topic.xpath("div/p/text()").extract_first()
+        for speaker_str in speakers_str.split(';'):
+          speaker = SpeakerItem()
+          (speaker['name'], speaker['corp']) = extract_speaker_info(speaker_str)
+          speaker['bio'] = ''
+          summit['speakers'].append(speaker)
 
         # send request to video page
-        video_request = Request(url=video_link, 
-                                meta={'item': item},
-                                cookies=gen_cookie_map(self.confluent_cookie_str), 
-                                callback=self.parse_video_page)
-        yield video_request
-
+        request = Request(url=video_link, 
+                          meta={'summit': summit},
+                          cookies=gen_cookie_map(self.confluent_cookie_str), 
+                          callback=self.parse_video_page)
+        yield request
+        #break
 
   def parse_video_page(self, response):
-    item = response.request.meta['item']
-    l = ItemLoader(item=SummitItem(), response=response)
-    (video_link, slide_link) = response.xpath("//div[@class='generic_content']/div/div/iframe/@src").extract()
-    item['video_link'] = video_link
-    item['slide_link'] = slide_link
-    yield item
+    summit = response.request.meta['summit']
+    l = ItemLoader(summit=SummitItem(), response=response)
+
+    link_list = response.xpath("//div[@class='generic_content']/div/div//iframe/@src").extract()
+    (video_link, slide_link) = ('', '')
+    if link_list:
+      (video_link, slide_link) = link_list
+
+    video = VideoItem()
+    video['src_link'] = video_link
+    video['dl_link'] = ''
+    slide = SlideItem()
+    slide['src_link'] = slide_link
+    slide['dl_link'] = ''
+    summit['video'] = video
+    summit['slide'] = slide
+    summit['base_fname'] = extract_basename(video_link)
+    yield summit
+
+
+      
+
+
